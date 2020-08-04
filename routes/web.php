9d2d7d824d97b6ee7d65c85f7923e4c9c1f3b458
Asa Marshall <asa.marshall@spinen.com>
2020-08-04 16:51:13 -0400

    Initial Commit: Starting after Episode 7 of "Laravel from Scratch"

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('test', [
+        'name' => request('name')
+    ]);
+
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/posts/{post}', function($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing'
+    ];
+
+    if (!array_key_exists($post, $posts)) {
+        abort(404, "Sorry that post was not found.");
+    }
+
+    return view('post', [
+        'post' => $posts[$post]
+//      'post' => $posts[$post] ?? "Nothing here yet." //add a default
+    ]);
+});
