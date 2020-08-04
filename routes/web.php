220e3a3d5e224d9e20184c0df582861c6c1a42e9
Asa Marshall <asa.marshall@spinen.com>
2020-08-04 17:35:18 -0400

    Episode 8: Routing to Controllers. - Create a controller using 'php artisan make:controller <name>' - Made a PostsController with artisan, and defined a function 'show' that displays information on the web page using code from the last episode

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,23 @@ Route::get('/test', function () {
     return view('test');
 });
 
-Route::get('/posts/{post}', function($post) {
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing'
-    ];
+//Method before controller
+//
+//Route::get('/posts/{post}', function($post) {
+//    $posts = [
+//        'my-first-post' => 'Hello, this is my first blog post!',
+//        'my-second-post' => 'Now I am getting the hang of this blogging thing'
+//    ];
+//
+//    if (!array_key_exists($post, $posts)) {
+//        abort(404, "Sorry that post was not found.");
+//    }
+//
+//    return view('post', [
+//        'post' => $posts[$post]
+////      'post' => $posts[$post] ?? "Nothing here yet." //add a default
+//    ]);
+//});
 
-    if (!array_key_exists($post, $posts)) {
-        abort(404, "Sorry that post was not found.");
-    }
-
-    return view('post', [
-        'post' => $posts[$post]
-//      'post' => $posts[$post] ?? "Nothing here yet." //add a default
-    ]);
-});
+//@show means showing a plug post
+Route::get('/posts/{post}', 'PostsController@show');
